165dbd01b3d4f6dbf8fae6cf9d10766a1e4e665a
Uedson Reis <uedsonreis@MacReis.local>
2020-01-16 21:22:26 -0300

    Tentando registrar o service-worker.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
@@ -17,7 +18,7 @@ export class AppComponent {
         { title: 'Tabela', url: '/table', icon: 'grid' }
     ];
 
-    constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
+    constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar, private swUpdate: SwUpdate) {
         this.initializeApp();
     }
 
@@ -25,6 +26,10 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+
+            setInterval(() => {
+                this.swUpdate.checkForUpdate();
+              } , 21600);
         });
     }
 
